d72d9d166df23f5cea87ad802f0223dc2b0b739d
davici0607@gmail.com <davici0607@gmail.com>
2025-01-31 11:50:14 +0700

    nambah profile

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -331,7 +331,6 @@
                 </div>
                         <h1>Bagian profile</h1>
                             <h4>hsllooo</h4>
-                            <h1>dvchisdlw</h1>
                 
                 </div>
             </main>
