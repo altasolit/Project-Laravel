cf11e6f025a5352bfb9dde676bca691e611fe62c
jon103 <altasolit>
2025-02-19 13:48:23 +0700

    tes

--- resources/views/reservasi.blade.php
+++ resources/views/reservasi.blade.php
@@ -64,28 +64,13 @@
                         </li>
                     </ul>
 
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                            aria-expanded="false">Pilihan Kamar</a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#Single Bed">Single Bed</a></li>
-                            <li><a class="dropdown-item" href="#Double Bed">Double Bed </a></li>
-                            <li><a class="dropdown-item" href="#Twin Single Bed">Twin Single Bed</a></li>
-                            <li><a class="dropdown-item" href="#Twin Double Bed">Twin Double Bed </a></li>
-                            <li><a class="dropdown-item" href="#VIP"><i><b><span style="color :gold">
-                                                VIP</span></b></i> Room</b></a></li>
-                        </ul>
-                    </li>
-                    </ul>
+
 
                     <form class="d-flex">
                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                         <button class="btn btn-outline-success" type="submit">Search</button>
                     </form>
-          <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
+
         </div>
       </div>
     </nav>
